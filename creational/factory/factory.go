289157e03fa08f9112ed11be34cdbb420810bac5
vaibhav malave <vaibhavmalave@ayopop.com>
2020-06-26 23:02:48 +0530

    done with factory patterns

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -5,21 +5,35 @@ import (
 	"fmt"
 )
 
+//constants for payment methods
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+//all payment methods should implement this interface
 type PaymentMethod interface {
 	Pay(amout float64) string
 }
 
-type Cash struct{}
-type DebitCard struct{}
+type CashPaymentMethod struct{}      //payment method for cash
+type DebitCardPaymentMethod struct{} //payment method for debit card
 
-func (c *Cash) Pay(amount float64) string {
-	return fmt.Sprintf("Payment of %0.2f paid using cash payment method.")
+func (c *CashPaymentMethod) Pay(amount float64) string {
+	return fmt.Sprintf("Payment of %0.2f paid using cash payment method.", amount)
 }
 
-func (c *DebitCard) Pay(amount float64) string {
-	return fmt.Sprintf("Payment of %0.2f paid using debit card payment method.")
+func (c *DebitCardPaymentMethod) Pay(amount float64) string {
+	return fmt.Sprintf("Payment of %0.2f paid using debit card payment method.", amount)
 }
 
 func GetPaymentMethod(paymentMethod int) (PaymentMethod, error) {
-	return nil, errors.New("Not implemented yet")
+	switch paymentMethod {
+	case 1:
+		return &CashPaymentMethod{}, nil
+	case 2:
+		return &DebitCardPaymentMethod{}, nil
+	default:
+		return nil, errors.New("Not implemented yet")
+	}
 }
--- creational/factory/factory_test.go
+++ creational/factory/factory_test.go
@@ -1,1 +1,48 @@
 package factory
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCashPaymentMethod(t *testing.T) {
+	var payment PaymentMethod
+	var err error
+	payment, err = GetPaymentMethod(Cash)
+
+	if err != nil {
+		t.Error("Cash payment method must be implemented.")
+	}
+
+	result := payment.Pay(10.2)
+	if !strings.Contains(result, "paid using cash payment method") {
+		t.Error("The cash payment method result wasn't correct")
+	}
+	t.Log("result", result)
+}
+
+func TestDebitCardPaymentMethod(t *testing.T) {
+	var payment PaymentMethod
+	var err error
+	payment, err = GetPaymentMethod(DebitCard)
+
+	if err != nil {
+		t.Error("Debit card payment method must be implemented.")
+	}
+
+	result := payment.Pay(10.2)
+	if !strings.Contains(result, "paid using debit card payment method") {
+		t.Error("The debit card payment method result wasn't correct")
+	}
+	t.Log("result", result)
+}
+
+func TestPaymentMethodDoesNotExist(t *testing.T) {
+	var err error
+	paymentMethod := 10
+	_, err = GetPaymentMethod(paymentMethod)
+
+	if err == nil {
+		t.Errorf("Payment method with id %d must return an error.", paymentMethod)
+	}
+}
